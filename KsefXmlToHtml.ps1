f7298e03d1b215b2bd5f8b0fc8a662bd087a0ffc
Grzegorz <oleksy@cdest.eu>
2026-02-18 12:24:31 +0100

    Start project

---
+++ KsefXmlToHtml.ps1
@@ -0,0 +1,36 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+$dialog = New-Object System.Windows.Forms.OpenFileDialog
+$dialog.InitialDirectory = 'F:\xsef'
+$dialog.Filter = 'Pliki XML (*.xml)|*.xml|Wszystkie pliki (*.*)|*.*'
+
+if ($dialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+    $xml = $dialog.FileName
+}
+
+
+$xsl = Join-Path $PSScriptRoot 'kseffaktura_fa(3).xsl'
+$out = Join-Path $PSScriptRoot 'faktura.html'
+$xmlNameNoExt = [System.IO.Path]::GetFileNameWithoutExtension($xml)
+$out = Join-Path $PSScriptRoot ($xmlNameNoExt + '.html')
+
+
+# tylko to jest potrzebne
+Add-Type -AssemblyName System.Xml
+
+$xslt = New-Object System.Xml.Xsl.XslCompiledTransform
+$xslt.Load($xsl)
+
+$settings = $xslt.OutputSettings.Clone()
+$settings.Indent = $true
+$settings.Encoding = [System.Text.UTF8Encoding]::new($false)
+
+$reader = [System.Xml.XmlReader]::Create($xml)
+$writer = [System.Xml.XmlWriter]::Create($out, $settings)
+
+$xslt.Transform($reader, $writer)
+
+$writer.Close()
+$reader.Close()
+
+Write-Host "Gotowe: $out"
